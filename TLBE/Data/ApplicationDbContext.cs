5682bfba695094a385364239e2f681612430fdbe
Joe <overtonje@gmail.com>
2021-03-01 16:35:27 -0600

    setting up dbcontext

---
+++ TLBE/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TLBE.Models;
+
+namespace TLBE.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Tune> Tune { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<Collection> Collection { get; set; }
+    }
+}
--- TLBE/Repositories/TuneRepository.cs
+++ TLBE/Repositories/TuneRepository.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TLBE.Models;
 
 namespace TLBE.Repositories
 {
     public class TuneRepository
     {
 
+        private readonly ApplicationDbContext _context;
+        public TuneRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public List<Tune> GetTunesByUserId()
+        { 
+        
+        }
         //get tunes by user (id)?
 
         //add tune 
