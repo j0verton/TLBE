98f4ab8679104e55c2080e63f5814ad2239c02d7
Joe <overtonje@gmail.com>
2021-03-15 14:55:32 -0500

    added toast dep and working on client auth

---
+++ TLBE/tlclient/src/components/auth/Login.js
@@ -0,0 +1,98 @@
+import React, { useRef } from "react"
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import { useHistory } from "react-router-dom"
+import { Button, Divider, Image } from "semantic-ui-react"
+import logo from "../../img/TuneListLogo.png"
+// import video from "https://res.cloudinary.com/banjo/video/upload/v1604000786/MOV04365_elnyos.mp4"
+import "./Login.css"
+
+export const Login = props => {
+    const { login, logout } = useContext(UserProfileContext);
+    const [loading, setLoading] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const history = useHistory();
+
+    const email = useRef()
+    const existDialog = useRef()
+    const history = useHistory()
+
+    const existingUserCheck = () => {
+        return fetch(`http://localhost:8088/users?email=${email.current.value}`)
+            .then(res => res.json())
+            .then(user => user.length ? user[0] : false)
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        setLoading(true);
+        login(email, password)
+            .then((user) => {
+                setLoading(false);
+                user.userStatusId === 2 ? toast.info('That account in inactive', logout())
+                    : toast.info(`Welcome back ${user.username}`, history.push("/"));
+            })
+            .catch((err) => {
+                setLoading(false);
+                toast.error("Invalid email or password");
+            });
+
+        // existingUserCheck()
+        //     .then(exists => {
+        //         if (exists) {
+        //             localStorage.setItem("tunes_user", exists.id)
+        //             history.push("/")
+        //         } else {
+        //             existDialog.current.showModal()
+        //         }
+        //     })
+    }
+
+    return (
+        <>
+            <div className="mainContainer">
+                <main className="container--login">
+                    {/* <dialog className="dialog dialog--auth" ref={existDialog}>
+                        <div>User does not exist</div>
+                        <button className="button--close" onClick={e => existDialog.current.close()}>Close</button>
+                    </dialog> */}
+
+                    <section>
+                        <form className="form--login" onSubmit={handleLogin}>
+                            <Image src={logo} size="medium" alt="TuneList logo, a fiddle over 3 sheets of paper" className="LogoNav" />
+                            <Divider />
+
+                            <p className="about">TuneList is an app that fiddle players can use to store and organize their tune list and tune information.</p>
+
+                            <Divider />
+
+                            <h2>Please sign in</h2>
+                            <fieldset>
+                                <label htmlFor="inputEmail"> Email address </label>
+                                <input ref={email} type="email"
+                                    id="email"
+                                    className="form-control"
+                                    placeholder="Email address"
+                                    required />
+                            </fieldset>
+                            <fieldset>
+                                <Button primary type="submit">
+                                    Sign in
+                        </Button>
+                            </fieldset>
+                        </form>
+                    </section>
+                    <section className="link--register">
+                        <Link to="/register">Not a member yet?</Link>
+                    </section>
+                </main>
+
+                <video className="videoTag" autoPlay loop muted>
+                    <source src={'https://res.cloudinary.com/banjo/video/upload/v1604000786/MOV04365_elnyos.mp4'} type="video/mp4" />
+                </video>
+            </div>
+        </>
+    )
+}
---
+++ TLBE/tlclient/src/components/nav/NavBar.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import { Icon, Image } from "semantic-ui-react"
+import logo from "../../img/TuneListLogo.png"
+import "./NavBar.css"
+
+export const NavBar = (props) => {
+    return (
+        <section className="navbar__container">
+
+            <ul className="navbar">
+                <li className="navbar__item navbar__logo active">
+                    <Link className="navbar__link" to="/">
+                        {/* <Header as="h2">Tune List</Header> */}
+                        <Image src={logo} floated="left" size="medium" alt="TuneList logo, a fiddle over 3 sheets of paper" className="LogoNav" />
+                    </Link>
+                </li>
+                <li className="navbar__item active">
+                    <Link className="navbar__link" to="/tunes"><Icon name="newspaper" /><p className="nav__text">Tunes</p></Link>
+                </li>
+                <li className="navbar__item active">
+                    <Link className="navbar__link" to="/users"><Icon name="address book" /><p className="nav__text">Users</p></Link>
+                </li>
+                {/* <li className="navbar__item active">
+                    <Link className="navbar__link" to="/messages"><Icon name="list alternate" /><p className="nav__text">Message Board</p></Link>
+                </li> */}
+                <li className="navbar__item active">
+                    <Link className="navbar__link" to="/tunes/create"><Icon name="add square" /><p className="nav__text">Add a Tune</p></Link>
+                </li>
+                <li className="navbar__item active">
+                    <Link className="navbar__link" to="/tunes/options"><Icon name="settings" /><p className="nav__text">Options</p></Link>
+                </li>
+                <li className="navbar__item active logout">
+                    <Link className="navbar__link" onClick={() => {
+                        localStorage.removeItem("tunes_user")
+                    }}
+                        to="/login"><Icon name="eject" /><p className="nav__text">Logout</p></Link>
+                </li>
+            </ul>
+        </section>
+    )
+}
