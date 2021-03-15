001d1aec199603490de54175a4bdfc9fe3e93de3
Joe <overtonje@gmail.com>
2021-03-15 10:16:23 -0500

    added appviews code from tl1

---
+++ TLBE/tlclient/src/ApplicationViews.js
@@ -0,0 +1,55 @@
+import React from "react"
+import { Route } from "react-router-dom"
+import { TuneForm } from "./components/tunes/TuneForm"
+import { TuneProvider } from "./components/tunes/TuneProvider"
+import { Home } from "./Home"
+import { ListCard } from "./components/tunes/ListCard"
+import { TunesList } from "./components/tunes/TuneListList"
+import { CollectionProvider } from "./components/collections/CollectionsProvider"
+import { UserProvider } from "./components/user/UserProvider"
+import { UserList } from "./components/user/UserList"
+import { TuningProvider } from "./components/tunings/TuningsProvider"
+import { PhotoProvider } from "./components/photo/PhotoProvider"
+import { OptionsList } from "./components/options/OptionsList"
+
+export const ApplicationViews = (props) => {
+    return (
+        <>
+            <CollectionProvider>
+                <TuneProvider>
+                    <UserProvider>
+                        <TuningProvider>
+                            <PhotoProvider>
+
+                                <Route exact path="/">
+                                    <Home />
+                                </Route>
+
+                                <Route exact path="/tunes/create">
+                                    <TuneForm />
+                                </Route>
+
+                                <Route exact path="/tunes/edit/:tuneId">
+                                    <TuneForm />
+                                </Route>
+
+                                <Route exact path="/tunes">
+                                    <TunesList />
+                                </Route>
+
+                                <Route exact path="/users">
+                                    <UserList />
+                                </Route>
+
+                                <Route exact path="/tunes/options">
+                                    <OptionsList />
+                                </Route>
+
+                            </PhotoProvider>
+                        </TuningProvider>
+                    </UserProvider>
+                </TuneProvider>
+            </CollectionProvider>
+        </>
+    )
+}
