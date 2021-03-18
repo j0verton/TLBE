66affd1b23f9ab047db01dcca3ae205b1199f94f
Joe <overtonje@gmail.com>
2021-03-18 11:00:30 -0500

    removed swagger openapi

--- TLBE/Startup.cs
+++ TLBE/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,10 +31,7 @@ namespace TLBE
             services.AddTransient<ITuningRepository, TuningRepository>();
             services.AddTransient<ITuneRepository, TuneRepository>();
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TLBE", Version = "v1" });
-            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,8 +40,7 @@ namespace TLBE
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TLBE v1"));
+
             }
 
             app.UseHttpsRedirection();
