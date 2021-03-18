fa8c50fea0234bc834a0f0df9a6ffebfd6f8163e
Joe <overtonje@gmail.com>
2021-03-18 11:40:46 -0500

    adde pk to collection

--- TLBE/Models/Collection.cs
+++ TLBE/Models/Collection.cs
@@ -7,6 +7,7 @@ namespace TLBE.Models
 {
     public class Collection
     {
+        public int Id { get; set; }
         public int UserId { get; set; }
         public string Name { get; set; }
         public List<Tune> Tunes { get; set; }
