9bd228dc35a31beb24da707ee7fde28878a5ae97
Joe <overtonje@gmail.com>
2021-03-24 14:12:35 -0500

    added some old code into collection provider

---
+++ TLBE/tlclient/src/providers/CollectionProvider.js
@@ -0,0 +1,42 @@
+import React, { useState, createContext } from "react"
+
+export const CollectionContext = createContext()
+
+export const CollectionProvider = props => {
+
+    const addCustomCollection = (collectionName) => {
+        return fetch('http://localhost:8088/collections', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                userId: parseInt(localStorage.getItem("tunes_user")),
+                name: collectionName,
+                custom: true
+            })
+        })
+    }
+
+    const getCustomCollectionsByUserId = (userId) => {
+        return fetch(`http://localhost:8088/collections/?userId=${userId}&custom=true&_embed=tuneCollections`)
+            .then(response => response.json())
+    }
+
+    const deleteCollection = collectionId => {
+        return fetch(`http://localhost:8088/collections/${collectionId}`, {
+            method: 'DELETE'
+        }).then(getCollections)
+    }
+
+    //delete unused Collections - need this somewhere, maybe server side
+
+
+    return (
+        <CollectionContext.Provider value={{
+            collections, tuneCollections, getCollections, saveCollection, deleteCollection, getCollectionsByUserId, getTuneCollectionsByCollectionIdWithTunes, deleteUnusedCollections, addCustomCollection, editCollection, getCustomCollectionsByUserId
+        }}>
+            {props.children}
+        </CollectionContext.Provider>
+    )
+}
